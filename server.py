3227ceb5b0798271194b46137c198a0b08e7d6db
A-Vamshi <vamshi.edustudy@icloud.com>
2026-03-12 17:25:02 +0700

    add logging for main entrypoint

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI, HTTPException, Request, Depends
 from mcp.server.sse import SseServerTransport
 from fastapi.middleware.cors import CORSMiddleware
+from mcp_logger import logger, AuditLoggingMiddleware
 
 
 from confident_mcp import mcp
@@ -25,16 +26,21 @@ async def set_auth(request: Request):
 
 sse = SseServerTransport("/messages")
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(AuditLoggingMiddleware)
 
-@app.get("/mcp", dependencies=[Depends(set_auth)]) # Usually used as /sse but using /mcp for convention
+@app.get("/mcp") # Usually used as /sse but using /mcp for convention
 async def handle_sse(request: Request):
     """Initial connection endpoint for the MCP client"""
+    await set_auth(request)
+
     async with sse.connect_sse(request.scope, request.receive, request._send) as streams:
         await mcp._mcp_server.run(streams[0], streams[1], mcp._mcp_server.create_initialization_options())
 
-@app.post("/messages", dependencies=[Depends(set_auth)])
+@app.post("/messages")
 async def handle_messages(request: Request):
     """Message passing endpoint for tool execution"""
+    await set_auth(request)
+
     await sse.handle_post_message(request.scope, request.receive, request._send)
 
 if __name__ == "__main__":
